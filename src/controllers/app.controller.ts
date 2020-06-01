300491bfe90323e7822e1d1a46cb05ba07dbe634
Vezbe PIVT <primer.aplikacija@gmail.com>
2020-06-01 18:25:52 +0200

    Do minor code fixes and optimizations

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AdministratorService } from '../services/administrator/administrator.service';
+
 
 
 @Controller()
 export class AppController {
-  constructor(
-    private administratorService: AdministratorService
-  ) {}
+  
 
   @Get()
   getIndex(): string {
